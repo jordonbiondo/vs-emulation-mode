e7803b3035900aa9de975cc5016afbd2236ac7ec
Fanael Linithien <fanael4@gmail.com>
2014-08-05 19:22:01 +0200

    Reformat the commentary.

--- vs-emulation-mode.el
+++ vs-emulation-mode.el
@@ -39,8 +39,12 @@
 
 ;; Minor mode providing a faithful emulation of a certain proprietary IDE.
 ;;
-;; Currently implemented features are: large memory footprint, random slowdowns,
-;; freezes, sending your data to the NSA and, last but not least, crashes.
+;; Currently implemented features:
+;; * large memory footprint
+;; * random slowdowns
+;; * random freezes
+;; * sending your data to the NSA
+;; * and last but not least, random crashes
 
 ;;; Code:
 
